1e14cf07e471e1b77caeb55da1ad12a2df85fe0c
Max <maxbretschneider@mailbox.org>
2022-09-06 18:17:38 +0200

    Added parseSchemaElement function and basic Args class structure

---
+++ Args.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner6;
+
+import static Args.Exception.ErrorCode.*;
+
+public class Args {
+    private Map<Character, ArgumentMarshaler> marshalers;
+    private Set<Character> argsFound;
+    private ListIterator<String> currentArgument;
+
+    public Args(String schema, String[] args) throws ArgsException {
+        marshalers = new HashMap<Character, ArgumentMarshaler>();
+        argsFound = new HashSet<Character>();
+
+        parseSchema(schema);
+        parseArgumentStrings(Arrays.asList(args));
+    }
+
+    private void parseSchema(String schema) throws ArgsException {
+        for (String element : schema.split(","))
+            if (element.length() > 0)
+                parseSchemaElement(element.trim());
+    }
+
+    private void parseSchemaElement(String element) throws ArgsException {
+        char elementId = element.charAt(0);
+        String elementTail = element.substring(1);
+        validateSchemaElementId(elementId);
+        if (elementTail.length() == 0)
+            marshalers.put(elementId, new BooleanArgumentMarshaler());
+        else if (elementTail.equals("*"))
+            marshalers.put(elementId, new StringArgumentMarshaler());
+        else if (elementTail.equals("#"))
+            marshalers.put(ElementId, new IntegerArgumentMarshaler());
+        else if (elementTail.equals("##"))
+            marshalers.put(elementId, new DoubleArgumentMarshaler());
+        else if (elementTail.equals("[*]"))
+            marshalers.put(elementId, new StringArrayArgumentMarshaler());
+        else
+            throw new ArgsException(INVALID_ARGUMENT_FORMAT, elementId, elementTail);
+
+    }
