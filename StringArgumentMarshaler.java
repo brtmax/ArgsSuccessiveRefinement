ebfcf237af062665e72f09973976ca5f2bb16dc1
Max <maxbretschneider@mailbox.org>
2022-09-06 19:25:38 +0200

    Added StringArgumentMarshaler source code

---
+++ StringArgumentMarshaler.java
@@ -0,0 +1,21 @@
+import static ArgsException.ErrorCode.*;
+
+public class StringArgumentMarshaler implements ARgumentMarshaler {
+    private String stringValue = "";
+
+    public void set(Iterator<String> currentArgument) throws ArgsException {
+        try {
+            stringValue = currentArgument.next();
+        } catch (NoSuchElementException e) {
+            throw new ArgsException(MISSING_STRING);
+        }
+    }
+
+    public static String getValue(ArgumentMarshaler am) {
+        if (am != null && am instanceof StringArgumentMarshaler) {
+            return ((StringArgumentMarshaler) am).stringValue;
+        } else {
+            return "";
+        }
+    }
+}
